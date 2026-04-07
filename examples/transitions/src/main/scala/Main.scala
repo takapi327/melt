75a5be86f4c7fe48ad464806d64c5c0ccdc8643c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2026-04-07 10:54:17 +0900

    Create transitions examples

---
+++ examples/transitions/src/main/scala/Main.scala
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2026 by Takahiko Tominaga
+ * This software is licensed under the Apache License, Version 2.0 (the "License").
+ * For more information see LICENSE or https://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import org.scalajs.dom
+
+import components.App
+
+@main def main(): Unit =
+  App.mount(dom.document.getElementById("app").asInstanceOf[dom.Element])
