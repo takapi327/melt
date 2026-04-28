4fca6f4791e0df3a928a798a9ae05336e4195a69
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2026-04-28 12:59:41 +0900

    Added Circe body codec

---
+++ modules/meltkit-adapter-http4s/shared/src/main/scala/meltkit/adapter/http4s/CirceBodyDecoder.scala
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2026 by Takahiko Tominaga
+ * This software is licensed under the Apache License, Version 2.0 (the "License").
+ * For more information see LICENSE or https://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package meltkit.adapter.http4s
+
+import io.circe.Decoder
+import io.circe.parser
+
+import meltkit.BodyError
+import meltkit.codec.BodyDecoder
+
+/** Provides a [[meltkit.codec.BodyDecoder]] for any type that has a Circe [[io.circe.Decoder]].
+  *
+  * Import the given instance to enable JSON body parsing in route handlers:
+  *
+  * {{{
+  * import meltkit.adapter.http4s.CirceBodyDecoder.given
+  *
+  * app.post("users") { ctx =>
+  *   ctx.body[CreateUserBody].flatMap {
+  *     case Right(body) => ...
+  *     case Left(err)   => IO.pure(ctx.badRequest(err))
+  *   }
+  * }
+  * }}}
+  */
+object CirceBodyDecoder:
+  given [A: Decoder]: BodyDecoder[A] with
+    def decode(body: String): Either[BodyError, A] =
+      parser.decode[A](body).left.map(e => BodyError.DecodeError(e.getMessage))
