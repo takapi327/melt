04068a3f5befe05d11abc86950a1436c87199fbb
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2026-07-11 23:21:09 +0900

    Action sbt scalafmtSbt

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,8 @@
-object ScalaVersions {
+object ScalaVersions:
   val scala3  = "3.3.7"
   val scala38 = "3.8.4"
-}
 
-object JavaVersions {
+object JavaVersions:
   val java17 = "17"
   val java21 = "21"
   val java25 = "25"
-}
